9085e74734e010f552e64a760845542d2acb778b
dricastroo <adrielle_dryk@hotmail.com>
2025-09-15 14:44:48 -0300

    Add files via upload

--- view/Main.java
+++ view/Main.java
@@ -1,14 +1,48 @@
-package view;
-import model.Aluno;
-import model.Professor;
-import model.Disciplina;
-public class Main {
-    public static void main(String[] args) {
-        Aluno aluno = new Aluno("João", "joao@email.com", 12345);
-        Professor professor = new Professor("Maria", "maria@email.com", "Matemática");
-        Disciplina disciplina = new Disciplina("Matemática", "MAT101");
-        System.out.println("Aluno: " + aluno.getNome() + " - Email: " + aluno.getEmail() + " - RA: " + aluno.getRa());
-        System.out.println("Professor: " + professor.getNome() + " - Email: " + professor.getEmail() + " - Disciplina: " + professor.getDisciplina());
-        System.out.println("Disciplina: " + disciplina.getNome() + " - Código: " + disciplina.getCodigo());
-    }
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+ import java.util.Scanner;
+ public class Main {
+
+     public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+
+         // Criando o aluno
+
+         System.out.print("Nome do aluno: ");
+         String nome = scanner.nextLine();
+
+         System.out.print("Quantas notas o aluno tem? ");
+         int quantidadeNotas = scanner.nextInt();
+
+         System.out.print("Qual o limite de faltas permitidas? ");
+         int limiteFaltas = scanner.nextInt();
+
+         Aluno aluno = new Aluno(nome, quantidadeNotas, limiteFaltas);
+
+         // Inserindo notas
+
+         for (int i = 0; i < quantidadeNotas; i++) {
+             System.out.print("Informe a nota " + (i + 1) + ": ");
+             double nota = scanner.nextDouble();
+             aluno.adicionarNota(i, nota);
+
+             // Inserindo faltas
+
+             System.out.print("Quantas faltas o aluno teve? ");
+             int faltas = scanner.nextInt();
+             aluno.adicionarFalta();
+
+             // Exibindo a situação do aluno
+
+             System.out.println("\n--- Situação do Aluno ---");
+             System.out.println("Nome: " + aluno.getNome());
+             System.out.println("Média: " + aluno.calcularMedia());
+             System.out.println("Faltas: " + aluno.getFaltas());
+             System.out.println("Situação: " + aluno.situacao());
+             scanner.close();
+
+         }
+
+     }
 }
