74441839f47be8e7e54dbdd5fa39341b754a4bb7
dricastroo <adrielle_dryk@hotmail.com>
2025-09-15 14:44:25 -0300

    Add files via upload

--- model/Aluno.java
+++ model/Aluno.java
@@ -1,16 +1,60 @@
-package model;
+public class Aluno {
 
-public class Aluno extends Pessoa {
-    private int ra;
+    private final String nome;
+    private final double[] notas;
+    private int faltas;
+    private final int limiteFaltas;
 
-    public Aluno(String nome, String email, int ra) {
-        super(nome, email);
-        this.ra = ra;
+    public Aluno(String nome, int quantidadeNotas, int limiteFaltas) {
+        this.nome = nome;
+        this.notas = new double[quantidadeNotas];
+        this.faltas = 0;
+        this.limiteFaltas = limiteFaltas;
     }
 
-    public int getRa() {
-        return ra;
+    public void adicionarNota(int indice, double nota) {
+        if (indice >= 0 && indice < notas.length) {
+            notas[indice] = nota;
+        } else {
+            System.out.println("Índice de nota inválido.");
+        }
+    }
+
+    public void adicionarFalta() {
+        this.faltas++;
+    }
+
+    public double calcularMedia() {
+        double soma = 0;
+        for (double nota : notas) {
+            soma += nota;
+        }
+        return soma / notas.length;
+    }
+
+    public boolean estaAprovado() {
+        return calcularMedia() >= 7.0 && faltas <= limiteFaltas;
+    }
+
+    public boolean reprovadoPorFalta() {
+        return faltas > limiteFaltas;
+    }
+
+    public String situacao() {
+        if (reprovadoPorFalta()) {
+            return "Reprovado por falta";
+        } else if (estaAprovado()) {
+            return "Aprovado";
+        } else {
+            return "Reprovado por nota";
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getFaltas() {
+        return faltas;
     }
 }
-
-    
